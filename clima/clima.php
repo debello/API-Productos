3bb55986ec8a427eed4358bd0227dc87ce86e226
debello <42223093+debello@users.noreply.github.com>
2019-05-15 16:15:04 +0200

    Styling Temperature

--- clima/clima.php
+++ clima/clima.php
@@ -2,50 +2,77 @@
 <html>
     <head>
         <style>
+            :root {
+                --main-box-color: hsla(0, 59%, 60%, 0.5);
+                --box-temp-color: hsl(172, 91%, 45%);
+                --border-temp-color: hsl(172, 91%, 30%);
+            }
+
             #bigbox {
                 display: grid;
-                grid-template-columns: 15vw 15vw 15vw 15vw;
-                grid-template-rows: 70px 50px 50px ;
+                grid-template-columns: 16vw 16vw 16vw 16vw;
+                grid-template-rows: auto auto auto ;
                 grid-template-areas:
-                " . dia ciudad . "
-                " . max max . "
-                " . min min . ";
-                grid-gap: 10px;
-                
-                justify-content: space;
+                " temp . . . "
+                " max . . . "
+                " min . . . ";                          
                 place-self: center;
                 
-                background-color: hsl(0, 50%, 50%);
+                background-color: var(--main-box-color);
                 width: auto;
                 height: 200px;
                 border-radius: 10px;
-                
             }
 
             #bigbox div p, #bigbox div {
                 display: grid;
-                place-self: center;
+                /* place-self: center; */
                 align-items: center;
+            }
+            #bigbox div > * {
+                display: grid;
+                grid-auto-flow: column;
+                place-self: center;
             }
 
             #bigbox div {
-                background-color: hsl(180, 50%, 50%);
-                width: 20vw;
-                height: 40px;
-                border-radius: 7px;
+                background-color: var(--box-temp-color);
+                width: auto;
+                height: auto;
+                /* border-radius: 5px; */
+                padding: 7px;
+                margin: 0px 7px 0px 5px;
+                border-top: 1.5px var(--border-temp-color) solid;
+                border-left: 1.5px var(--border-temp-color) solid;
+                border-right: 1.5px var(--border-temp-color) solid;
+                font-family: Arial;
+                color: hsl(10, 0%, 95%);
+            }
+            #bigbox div:nth-child(1) { /* Primera casilla vertical */
+                border-radius: 5px 5px 0px 0px;
+            }
+            #bigbox div:nth-child(3) { /* Ultima casilla vertical */
+                border-radius: 0px 0px 5px 5px;
             }
 
-                #box-dia {
+                /* #box-dia {
                     grid-area: dia;
                 }
                 #box-ciudad {
                     grid-area: ciudad;
+                } */
+
+                #box-temp {
+                    grid-area: temp;
                 }
                 #box-temp-max {
                     grid-area: max;
                 }
                 #box-temp-min {
                     grid-area: min;
+                }
+                .font-bold {
+                    font-weight: bold;
                 }
 
         </style>
@@ -80,24 +107,39 @@
     'Content-type:application/json'));
     //executar con curl_exec()
     $data = curl_exec($cliente2);
-
     $dataDecoded = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $data));
+    
+    /* Saber qué número de día de la semana es Hoy */ 
+    function saber_dia($nombredia) {
+        $dias = array('Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado');
+        $fecha = $dias[date('N', strtotime($nombredia))];
+        $fechaKey = array_search($fecha, $dias);
+        return $fechaKey;
+        }
+    // Ejecutamos la función pasándole la fecha que queremos
+    $numDiaHoy = saber_dia($dataDecoded[0]->elaborado);
+    
     ?>
 
     <div class="sb1">Testing sb1</div>
     <h2>TuClima.com</h2>
+    <p><?php echo 'Día: <span class="font-bold">' . $dataDecoded[0]->elaborado . "</span> Ciudad: <span class='font-bold'>" . utf8_encode($dataDecoded[0]->provincia) . "</span><br>"; ?></p>
+
     <div id='bigbox'>
-        <div id='box-dia'> 
-            <p>Día: <?php echo $dataDecoded[0]->elaborado . "<br>"; ?></p>
+        <!-- <div id='box-dia'> 
+            
         </div>
         <div id='box-ciudad'>
-            <p>Ciudad: <?php echo utf8_encode($dataDecoded[0]->provincia) . "<br>"; ?></p>
-        </div>  
+            
+        </div>   -->
+        <div id='box-temp'>
+            <p class='font-bold'>Temperatura</p>
+        </div>
         <div id='box-temp-max'>
-            <p>Temperatura Máxima: <?php echo $dataDecoded[0]->prediccion->dia[0]->temperatura->maxima . "<br>"; ?></p>
+            <p>Máx. <span class='font-bold'><?php echo $dataDecoded[0]->prediccion->dia[$numDiaHoy]->temperatura->maxima . "</span>"; ?></p>
         </div>  
         <div id='box-temp-min'>
-            <p>Temperatura Mínima: <?php echo $dataDecoded[0]->prediccion->dia[0]->temperatura->minima . "<br>"; ?></p>
+            <p>Mín. <span class='font-bold'><?php echo $dataDecoded[0]->prediccion->dia[$numDiaHoy]->temperatura->minima . "</span>"; ?></p>
         </div>  
     </div>
     
